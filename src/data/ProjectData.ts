8749c020a6a0655dd3e909b534800c0bd5f56b84
Rachel Lowe <60523642+11BelowStudio@users.noreply.github.com>
2023-10-19 23:38:00 +0100

    added a reminder about how to use ProjectData.ts
    also tried to dismiss some linter warnings

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -7,6 +7,16 @@ export default class ProjectData {
     isHigh: boolean; // thumbnail will take 2 rows in the grid view
     accentColor: string; // color of title bar 
 
+    /**
+     * constructor for the project data
+     * @param id unique ID for the project.
+     * @param name Display name for the project
+     * @param iconUrl URL for project thumbnail
+     * @param html HTML for the content of the project data. 
+     * @param accentColor colour of title bar
+     * @param isHigh if true, thumbnail will take 2 cols
+     * @param isWide if true, thumbnail will take 2 rows
+     */
     constructor(id: string, name: string, iconUrl: string, html: string, accentColor = "#000000", isHigh = false, isWide = false){
         this.id = id;
         this.name = name;
