b018af38351a34b7b7d9954dee228b0f190ad745
Rachel Lowe <60523642+11BelowStudio@users.noreply.github.com>
2023-10-23 20:24:06 +0100

    updated links in ContactPage

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -6,26 +6,31 @@
     <div style="margin-bottom:40px;">I'm looking for a job !<br/>Feel free to contact me about any opportunity, or just to chat about cooking, kittens or anything else.</div>
 
     <ul>
+      <!--
       <li>
         <i class="fa fa-user-circle-o fa-lg fa-fw"></i>
         <a href="http://mywebsite.com" target="_blank">https://mywebsite.com</a>
       </li>
+    -->
+    <!--
       <li>
         <i class="fa fa-envelope-o fa-lg fa-fw"></i>
         <a href="mailto:my@email.com" target="_blank">my@email.com</a>
       </li>
+    -->
       <li>
         <i class="fa fa-linkedin fa-lg fa-fw" style="vertical-align: -5%;"></i>
-        <a href="https://www.linkedin.com/me" target="_blank">linkedin.com/me</a>
+        <a href="https://www.linkedin.com/rachel-m-lowe" target="_blank">linkedin.com/rachel-m-lowe</a>
       </li>
       <li>
         <i class="fa fa-github fa-lg fa-fw"></i>
-        <a href="https://github.com/me" target="_blank">github.com/me</a>
+        <a href="https://github.com/11BelowStudio" target="_blank">github.com/11BelowStudio</a>
       </li>
       <li>
         <i class="fa fa-gamepad fa-lg fa-fw"></i>
-        <a href="https://myself.itch.io" target="_blank">myself.itch.io</a>
+        <a href="https://11BelowStudio.itch.io" target="_blank">11BelowStudio.itch.io</a>
       </li>
+      <!--
       <li>
         <i class="fa fa-tumblr fa-lg fa-fw"></i>
         <a href="https://myblog.tumblr.com" target="_blank">myblog.tumblr.com</a>
@@ -34,6 +39,7 @@
         <i class="fa fa-steam fa-lg fa-fw"></i>
         <a href="https://steamcommunity.com/id/me" target="_blank">steam.com/me</a>
       </li>
+      -->
     </ul>
 
   </div>
