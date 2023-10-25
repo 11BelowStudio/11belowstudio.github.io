846971efef071161aa71620d719ce26902733ff2
Rachel Lowe <60523642+11BelowStudio@users.noreply.github.com>
2023-10-25 14:45:36 +0100

    little bit more progress on the resume page

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -4,17 +4,17 @@
 
     <div class="paragraph">
       <div>
-        I'm <strong>John Matrix</strong>, a former Delta Force operative, now a hobbyist lumberjack.<br/>
-        I spend my days living alone with my daughter Jenny, and cutting trees and carrying trunks around to keep in shape.
+        I'm <strong>Rachel</strong>, a Computer Games graduate who makes games (designing and implementating them) and solves problems.<br/>
+        Nowadays I'm mostly preoccupied with navigating the job market, whilst doing some volunteering in the community.
       </div>
 
-      <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">stuff</router-link>, on <router-link to="/other-projects">other stuff</router-link>, and took part in <router-link to="/resume">a few things</router-link> as well.</div>
+      <div style="margin-top: 20px;">I've <router-link to="/game-projects">made games</router-link>, dabbled in <router-link to="/other-projects">non-games</router-link>, and you can see <router-link to="/resume">my résumé</router-link> as well.</div>
 
-      <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a monk, like my good friend John Rambo did a few years back. You can reach me at <a href="mailto:johnmatrix@deltaforce.us">johnmatrix@deltaforce.us</a> or <router-link to="/contact">through here</router-link>.</div>
+      <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> - ideally either as a game programmer or a game designer. You can reach me <!--at <a href="mailto:johnmatrix@deltaforce.us">johnmatrix@deltaforce.us</a> or --><router-link to="/contact">through here</router-link>.</div>
     </div>
 
     <div class="photo">
-      <img :src="`/img/avatar.png`" alt="Avatar of John" /> 
+      <img :src="`/img/avatar.png`" alt="11BelowStudio logo" /> 
     </div>
 
     <div style="clear:both"></div>
--- src/views/ResumePage.vue
+++ src/views/ResumePage.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
-    <h1>Resume</h1>
+    <h1>Résumé</h1>
 
     <div class="paragraph">
+
       I'm <strong>John Matrix</strong>, a former Delta Force operative with lots of experience, lots of training and huge muscles.
       <br />After graduating from kindergarten, I've been enrolled in the army for 25 years, where I learned to destroy things in an amazing number of ways.
       <br />I joined the Delta Force because I thought it was Greek lessons, but I quickly adapted to this unexpected environment and became a key element.
@@ -21,6 +22,7 @@
     </div>
     <div class="photo">
       <img :src="`/img/resume-photo.png`" alt="Rachel" />
+      <p>caption caption caption</p>
     </div>
 
     <div style="clear:both"></div>
