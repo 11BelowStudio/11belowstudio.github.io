2874df32768f1e763af0e5cb3f3f083c576098b6
Rachel Lowe <60523642+11BelowStudio@users.noreply.github.com>
2024-02-07 17:48:15 +0000

    adding Feathersword's Revenge to the portfolio

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -3,6 +3,111 @@ import ProjectDataCollection from '@/data/ProjectDataCollection';
 
 export default new ProjectDataCollection([
 
+
+  new ProjectData(
+    "project-5",
+    "Feathersword's Revenge",
+    "img/projects/feathersword-icon.gif",
+    `
+    <div class="paragraph">
+      <strong>Feathersword's Revenge</strong>
+      be a swashbucklin', rib-ticklin', physics-'avin game for PC, created wit' Unity.<br/>
+      This game was made for the <a href="https://globalgamejam.org/games/2024/feathersword-1" target="_blank"><em>2024 Global Game Jam</em></a> (January 2024)
+      - the theme of which being <em>Make Me Laugh</em> - 
+      natually leading to the development of a game in which you make
+      people laugh by <span class="reveal"><span class="opposite-hide"><em>TICKLING</em> them</span><span class="hide">bashing them over the head</span></span> with a
+      comically large feather, whilst not getting <span class="reveal"><span class="opposite-hide"><em>TICKLED</em></span><span class="hide">bashed over the head</span></span> yourself.<br/>
+      I worked on this with a team of five other people on the <a href="https://www.gamedev.london/" target="_blank">Game Dev London</a> discord server
+      (all of us were working remotely), however, I was responsible for almost all of the implementation of the game. This game came in 8th place overall in the
+      <a href="https://itch.io/jam/gdl-ggj24-community-showcase/rate/2495090" target="_blank">Game Dev London GGJ24 community showcase</a>.
+    </div>
+
+    <div class="paragraph">
+      <div class="notice notice-container">
+        <div class="notice-text-square">
+          <cite>Feathersword's Revenge</cite> be playable on <a href="https://11belowstudio.itch.io/feathersword" target="_blank">itch.io</a>.<br/>
+          It be playable in t' browser fer lazy pirates, and it be downloadable fer Windows and Linux.<br/>
+          Source code be viewable <a href="https://github.com/11BelowStudio/GGJ2024" target="_blank">on t' Hub o' t' Gits</a>.<br/>
+          T' Global Game Jam submission be viewable <a href="https://globalgamejam.org/games/2024/feathersword-1" target="_blank">on t' GGJ website</a>.
+        </div>
+        <div class="notice-itch-square">
+          <iframe frameborder="0" src="https://itch.io/embed/2495090?linkback=true&amp;bg_color=222222&amp;fg_color=eeeeee&amp;border_color=363636" width="100%" height="167">
+          <a href="https://11belowstudio.itch.io/feathersword" target="_blank">Feathersword's Revenge by 11BelowStudio, Kobby6-lang, jujubee0728, A Big Owl</a>
+          </iframe>
+        </div>
+      </div>
+    </div>
+
+    <div class="paragraph">
+      Main features:
+      <ul class="features-list">
+        <li>Intentionally wonky physics-based characters working in tandem with physics-based attack damage system - physical comedy as a game mechanic.</li>
+        <li>AI enemies (with physics <em>and</em> pathfinding) who get stronger as you defeat more waves of them!</li>
+        <li>Tastefully scuffed visuals, immersive sound effects, great music, and an overall cohesive gameplay experience
+        - with a comedic ludonarratively dissonant "aggressively non-violent" presentation.</li>
+      </ul>
+    </div>
+
+    <div class="paragraph center">
+      <img class="pc-screenshot" src="img/projects/feathersword/screenshot1.png" alt="The Captain be walkin' toward a distant foe" />
+      <img class="pc-screenshot" src="img/projects/feathersword/screenshot3.png" alt="The Captain be assertin' dominance o'er several foes who be on t' ground, laughing uncontrollably" />
+      <img class="pc-screenshot" style="max-height:225px;max-width:min(100%,284px);width:auto;" src="img/projects/feathersword/preview-gif.gif" alt="A rather short gif with some gameplay footage of Feathersword's Revenge" />
+    </div>
+
+
+    <div class="paragraph">
+      As stated above, this game wasn't entirely made by myself - however, due to a lack of programmers on the team, I was responsible for almost all
+      of the actual implementation of this game (whilst the other team members handled most of the asset creation).<br/>
+      The design of the game came about as a combination of various suggestions from the team for game ideas.
+      One member of the team suggested 'making something laugh by tickling it', followed by another person suggesting the use of a feather,
+      at which point my brain jumped to <a href="https://en.wikipedia.org/wiki/Captain_Feathersword" target="_blank"><i>Captain Feathersword</i> from <cite>The Wiggles</cite></a>,
+      and so I pitched the idea of this game - and we ultimately chose to work on this concept.
+      The ragdoll physics were included as a result of another team member suggesting 'clumsiness' in the prior conversation.
+      <span class="reveal">More info <span class="opposite-hide">(hover)</span><br/>
+      <span class="hide">Initially, I concieved this game as some Totally Real Lost Media from <cite>The Wiggles</cite>,
+      in which Captain Feathersword (from <cite>The Wiggles</cite>) would be lootin' treasure from some scallywags in t' seven seas,
+      wit' all t' violence needed to defeat those scurvy dogs - but presented in a "family friendly" way via overzealous whitewashing
+      of the violence (you <em>TICKLE</em> your <em>FRIENDS</em> with your <em>FEATHERSWORD</em> to make them fall to the ground in <em>UNCONTROLLABLE LAUGHTER</em>)
+      - with the meta-narrative of the game being that it became lost media due to The Wiggles promptly forcing development to cease as soon as they found out about the violence in the game.
+      This proposal ended up on the cutting room floor, but it may or may not still be part of the meta-narrative of the finished product 🙃.</span></span>
+    </div>
+
+    
+    <div class="paragraph">
+      <cite>Feathersword's Revenge</cite> is a single-player wave survival sort of game - in which waves of enemy pirates spawn in to <em>TICKLE</em> you,
+      (with the enemy count and AI parameters getting higher with each wave), and you must <em>TICKLE</em> all of them
+      (making them fall <em>LAUGHING UNCONTROLLABLY</em>) before they can do the same to you - and see how many waves you can survive.
+      This differed from the original plan of 'looting treasure from enemy pirates' mostly due to the time constraints of the game jam making
+      a level-based structure to the game rather unfeasible to fully design and implement, but 'surviving endless waves of AI enemies' was
+      - but it still was a challenge to get the game implemented.
+    </div>
+    
+    <div class="paragraph">
+      Due to our team all working remotely, being complete strangers, and not all knowing how to use various collaboration/creation tools,
+      there were some aspects of the development process which could have gone better. For example, whilst I opted to jump straight in with
+      creating a git repository, making the Unity project, and implementing the prototype after we agreed on the concept
+      (as I had been designated as the de facto implementation person, being the sole programmer on the team), we didn't get around to
+      planning what everyone else would be doing, which delayed the asset creation process due to people not knowing what assets they needed to make,
+      and caused some other delays later on in development due to the eternal problem of 'not knowing if someone else was already doing the thing'.
+      One victim of this was the custom player model which one team member managed to create, based on some concept art produced by another team member -
+      unfortunately, due to extreme time constraints, we weren't able to fully sort out the rigging on it and use it as the player model,
+      rendering it completely unused within the finished product. Additionally, most of the people in the team were unfamiliar with version control
+      systems, so several assets had to be sent via discord (and downloaded/added to the project manually) whilst doing it all via a VCS system would have
+      streamlined the process somewhat.
+    </div>
+
+    <div class="paragraph">
+      Still, nitpicks aside, I still think that <cite>Feathersword's Revenge</cite> was a success. The game has a full gameplay loop,
+      it functions mostly as intended, fits the theme of the game jam (the game is about making people laugh), and it's rather fun,
+      with some great audio (produced by the rest of the team) as well. Not bad for 48 hours!
+    </div>
+    `,
+    "#6D755D",
+    false,
+    false,
+    ["Unity","C#","PC","physics","game jam","browser"]
+  ),
+
   new ProjectData(
     "project-0",
     "Committy",
@@ -213,7 +318,7 @@ export default new ProjectDataCollection([
       Finally, the overarching development cycle - Whilst I did have <cite>CrabFight</cite>
       as a basis (meaning that I didn't need to rewrite the whole main menu and backend-y multiplayer management components),
       I still had a very limited amount of time in which to design the game (whilst ensuring that the stakeholders
-      approved of the design), sort out the assets, and develop the game.
+      approved of the design), sort out the assets, and develop the game (and dealing with some family issues which arose during this time).
     </div>
     `,
     "#cf142b",
@@ -358,7 +463,7 @@ export default new ProjectDataCollection([
     "#09162A",
     false,
     false,
-    ["Java","custom engine","PC"]
+    ["Java","custom engine","physics","PC"]
   )
 /*
   new ProjectData(
